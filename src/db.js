4efda34b6407b009ef215fbec2e675e3c7bd7772
Kwabena Boakye <kwaboakye93@gmail.com>
2025-03-02 22:37:55 +0000

    added database

---
+++ src/db.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+});
+
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error("Database connection failed:", err);
+    } else {
+        console.log("Connected to MySQL database!");
+        connection.release(); // Release the connection back to the pool
+    }
+});
+
+module.exports = pool.promise();
